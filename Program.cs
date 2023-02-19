f639d72d416904d1e3d8b05f8c4e443b8c423378
islom1994 <islommuxammad1994@gmail.com>
2023-02-20 03:40:20 +0500

    dz19.21.23

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 19: Напишите программу,
+// которая принимается на пятизначное число и возникает,
+// если оно палиндромом.
+// 14212 -> нет
+// 23432 -> да
+// 12821 -> да
+
+// Console.Write("Введите число: ");
+// int number = int.Parse(Console.ReadLine()!);
+// string str = null;
+//   if( number == number || number == number)
+//   {
+//  Console.WriteLine($"Ваше число: {number} - палиндром.");
+//  }
+// else 
+//     {
+//       Console.WriteLine($"Ваше число: {number} - НЕ палиндром.");
+//       }
+// if (number == 5)
+// { 
+//   Console.WriteLine($"Введите правильное число");
+//   }
+ 
+
+// // Задача 21: Напишите программу,
+// которая принимает вход в координаты двух точек и
+// нахождение расстояния между ними в 3D-пространствах.
+// А (3,6,8); В (2,1,-7), -> 15,84
+// А (7,-5, 0); В (1,-1,9) -> 11,53
+
+// Console.WriteLine("Введите Кординаты x1 :  ");
+// int x1 = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите Кординаты y1 :  ");
+// int y1 = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите Кординаты z1 :  ");
+// int z1 = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите Кординаты x2 :  ");
+// int x2 = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите Кординаты y2 :  ");
+// int y2 = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите Кординаты z2 :  ");
+// int z2 = int.Parse(Console.ReadLine()!);
+// int A = (x2-x1)*(x2-x1);
+// int B = (y2-y1)*(y2-y1);
+// int C = (z2-z1)*(z2-z1);
+// double ABC = Math.Sqrt(A+B+C);
+// Console.WriteLine(ABC); 
+
+
+
+// //Задача 23: Напишите программу,
+// которая принимает на вход число (N) и выдает
+// таблетку кубов числами от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+// Console.Write("Введите число: ");
+// int N = int.Parse(Console.ReadLine()!);
+// int x = 1;
+// while(x <= N )
+// {
+// int y = x*x;
+// Console.WriteLine(y);
+// x++;
+// }
+
+
+
